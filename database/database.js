9c847a3bc724aa12f4ffdc81281832dc220b72d2
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2022-01-11 15:01:55 +0600

    Connected to a database and created a collection for users reminders.

---
+++ database/database.js
@@ -0,0 +1,31 @@
+// imports
+const mongoose = require("mongoose");
+require("dotenv").config(); // Getting all the environment variables.
+
+// Database connection
+mongoose.connect(
+  `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@main-shard-00-00.pd3ns.mongodb.net:27017,main-shard-00-01.pd3ns.mongodb.net:27017,main-shard-00-02.pd3ns.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ly6s4r-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection ERROR :(:"));
+db.once("open", function () {
+  console.log("Database connected");
+});
+
+const reminderSchema = new mongoose.Schema({
+  setTime: {
+    type: Date,
+    default: Date.now
+  },
+  remindTime: {
+    type: Date,
+    required: true
+  },
+  reminderText: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports.Reminder = mongoose.model('user', reminderSchema);
