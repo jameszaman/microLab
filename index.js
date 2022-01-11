8361322923a1190b2716c5f2ed92a6682facc5af
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2022-01-11 14:35:09 +0600

    Setting up proper routes, templating and static folder.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express');
+// All External module imports.
+const express = require("express");
 
+// The main object we will work with.
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({'Name': 'James'});
-})
+// requiring routers.
+const baseRouter = require("./routes/baseRouter");
 
+// Setting static folders.
+app.use(express.static("public"));
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/images", express.static(__dirname + "/public/images"));
+// Showing favicon.
+app.use("/favicon.ico", express.static(__dirname + "/favicon.ico"));
+app.set("view engine", "ejs");
+
+// Using the routers to set up routes.
+app.use("/", baseRouter);
 
 // Starting the server.
 const port = process.env.PORT || 3000;
@@ -13,4 +25,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running. 😁`);
 });
-
